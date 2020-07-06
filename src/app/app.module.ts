11409bd9ef74fe968dffda0289310b0fe5bddad1
Bruno <bruno@leaoteixeira.com.br>
2020-07-05 22:33:43 -0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { FileEncryption } from '@ionic-native/file-encryption/ngx';
+import {FileTransfer} from '@ionic-native/file-transfer/ngx';
+import {FileChooser} from '@ionic-native/file-chooser/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +19,12 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    FileEncryption,
+    FileTransfer,
+    File,
+    FilePath,
+    FileChooser,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {FileTransfer, FileUploadOptions, FileTransferObject} from '@ionic-native/file-transfer/ngx';
+import {FileChooser} from '@ionic-native/file-chooser/ngx';
+import {FilePath} from '@ionic-native/file-path/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { AlertController } from '@ionic/angular';
+import { FileEncryption } from '@ionic-native/file-encryption/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +12,75 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tituloLivros: ["Minimos Contos","Dom Casmurro","Robson Crusoé"]
+  key: any = 'senhaarvore';
+  fileTransfer: FileTransferObject;
+  uploadText: any;
+  downlaodText: any;
+  constructor(private transfer: FileTransfer,private fileEncryption: FileEncryption,  private alertController: AlertController,
+    private file: File, private filePath: FilePath, private fileChooser: FileChooser) {
+      this.uploadText = "";
+      this.downlaodText = "";
+      console.log(this.encrypt())
+  }
+  
 
-  constructor() {}
+  encrypt() {
+    var file = '9788565126014.pdf';
+    this.fileEncryption.encrypt(file, 'secretKey').then(data=>{
+      return data;
+    })
+  }
 
+  async baixar() {
+    const alert = this.alertController.create({message: 'Arquivo encriptado baixado, transfira por bluetooth e faça o upload no app.',buttons: ['OK']});
+    (await alert).present();
+  }
+
+  async upload() {
+    this.fileChooser.open().then((uri) =>{
+      this.filePath.resolveNativePath(uri).then((nativePath) => {
+        this.fileTransfer = this.transfer.create();
+        let options:FileUploadOptions = {
+          fileKey: 'book',
+          fileName: 'name.pdf',
+          chunkedMode: false,
+          headers: {},
+          mimeType: 'book/pdf'
+        }
+        alert("Arquivo recebido")
+        this.uploadText = "enviando...";
+        this.fileTransfer.upload(nativePath,'/home',options).then((data)=>{
+          alert("transfer done = " + JSON.stringify(data));
+          this.uploadText = "";
+        },(err)=>{
+          this.uploadText = "";
+        })
+      },(err)=>{
+        alert(JSON.stringify(err));
+      })
+    },(err)=> {
+      alert(JSON.stringify(err));
+    })
+  }
+
+  abortUpload(){
+    this.fileTransfer.abort();
+    alert("upload cancel.");
+  }
+
+  async download(){
+    this.fileTransfer = this.transfer.create();
+    this.downlaodText = "downloading...";
+    let url = encodeURI('9788565126014.pdf');  
+    alert("Livro baixado");
+    console.log(url)
+    this.fileTransfer.download(url,this.file.externalRootDirectory).then((data)=>{
+      console.log("Data:" + data)
+      alert("download complete");
+      this.downlaodText ="";
+    },(err)=>{
+      console.log(err);
+    })
+  }
 }
